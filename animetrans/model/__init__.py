97989c250cfce1bcea84946fccdade2bef2658ea
narugo1992 <narugo1992@deepghs.org>
2025-11-01 18:39:37 +0800

    dev(narugo): save that

--- animetrans/model/__init__.py
+++ animetrans/model/__init__.py
@@ -1,2 +1,3 @@
 from .ensure_timm import ensure_timm_dependency
+from .load import load_model, load_model_from_timm, load_model_from_timm_transformers
 from .timm_model import TimmModelConfig, TimmModel
---
+++ animetrans/model/load.py
@@ -0,0 +1,80 @@
+import json
+from typing import Optional
+
+from huggingface_hub import hf_hub_download
+from timm.models import parse_model_name
+from transformers import AutoModel
+
+from .timm_model import TimmModelConfig, TimmModel
+
+
+def _get_timm_repo_id(timm_model_name: str):
+    model_source, model_name = parse_model_name(timm_model_name)
+    if model_source == 'hf-hub':
+        return model_name
+    else:
+        return f'timm/{model_name}'
+
+
+def load_model_from_timm(
+        model_name: str, task_type: str = 'classification', task_config: Optional[dict] = None,
+        unbind_from_src_repo: bool = True, **model_args,
+):
+    model_args = dict(model_args or {})
+    task_config = dict(task_config or {})
+    origin_model_cfg = TimmModelConfig(
+        model_name=model_name,
+        model_args=model_args,
+        task_type=task_type,
+        task_config=task_config,
+    )
+    origin_model = TimmModel(origin_model_cfg, pretrained=True)
+
+    if unbind_from_src_repo:
+        repo_id = _get_timm_repo_id(model_name)
+        with open(hf_hub_download(
+                repo_id=repo_id,
+                repo_type='model',
+                filename='config.json'
+        ), 'r') as f:
+            config = json.load(f)
+
+        model_cfg = TimmModelConfig(
+            model_name=config['architecture'],
+            model_args={**model_args, **(config.get('model_args') or {})},
+            task_type=task_type,
+            task_config=task_config,
+        )
+        model = TimmModel(model_cfg, pretrained=False)
+        model.load_state_dict(origin_model.state_dict())
+    else:
+        model = origin_model
+
+    return model
+
+
+def load_model_from_timm_transformers(
+        model_name: str, task_type: Optional[str] = None, task_config: Optional[dict] = None,
+):
+    origin_model = AutoModel.from_pretrained(model_name, pretrained=True)
+    assert type(origin_model).__name__ == 'TimmModel', \
+        f'Unsupported source transformers model - {model_name!r}.'
+    config = TimmModelConfig(
+        model_name=origin_model.config.model_name,
+        model_args=origin_model.config.model_args,
+        task_type=task_type or origin_model.config.task_type,
+        task_config=task_config or origin_model.config.task_config,
+    )
+    if origin_model.config.num_outputs != config.num_outputs:
+        origin_model.model.reset_classifier(config.num_outputs)
+
+    model = TimmModel(config, pretrained=False)
+    model.load_state_dict(origin_model.state_dict())
+    return model
+
+
+def load_model(model_name: str, task_type: Optional[str] = None, task_config: Optional[dict] = None, **kwargs):
+    if '/' not in model_name or 'hf-hub:' in model_name:
+        return load_model_from_timm(model_name, task_type=task_type, task_config=task_config, **kwargs)
+    else:
+        return load_model_from_timm_transformers(model_name, task_type=task_type, task_config=task_config)
