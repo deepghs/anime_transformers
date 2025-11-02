1ebc47fa6cef36032f09d8dd316825fab8db2383
narugo1992 <narugo1992@deepghs.org>
2025-11-03 00:33:19 +0800

    dev(narugo): save them

--- animetrans/classification/train.py
+++ animetrans/classification/train.py
@@ -1,32 +1,33 @@
-from typing import Optional
+import json
+import os.path
+import random
+from typing import Optional, Tuple
+
+from accelerate import Accelerator
+from accelerate.utils import broadcast_object_list
+from ditk import logging
+from hbutils.random import global_seed
+
+from .dataset import load_classes
+from ..dataset import load_pretrained_tag
+from ..model import ModelStep
 
 
 def train(
         workdir: str,
         dataset_repo_id: str,
-        timm_model_name: str,
+        model_name: str,
         num_workers: int = 16,
-        max_epochs: int = 100,
-        batch_size: int = 16,
+        max_epochs: int = 50,
+        batch_size: int = 32,
         learning_rate: float = 2e-4,
         weight_decay: float = 1e-3,
         key_metric: str = 'macro_f1',
         seed: Optional[int] = 0,
         eval_epoch: int = 1,
-        eval_threshold: float = 0.4,
         model_args: Optional[dict] = None,
-        pretrained_cfg: Optional[dict] = None,
 
-        noise_level: int = 2,
-        rotation_ratio: float = 0.0,
-        mixup_alpha: float = 0.6,
-        cutout_max_pct: float = 0.0,
-        cutout_patches: int = 0,
-        random_resize_method: bool = True,
-        pre_align: bool = True,
-        align_size: int = 512,
-        tag_categories: Optional[Sequence[int]] = None,
-        seen_tag_keys: Optional[List[str]] = None,
+        class_key: str = 'class',
         image_key: str = 'webp',
         use_pretrained_weight: bool = True,
         adam_betas: Optional[Tuple[float, float]] = None,
@@ -47,3 +48,38 @@ def train(
     # https://hansbug.github.io/hbutils/main/api_doc/random/state.html#register-random-source
     logging.info(f'Globally set the random seed {seed!r} in process #{accelerator.process_index}.')
     global_seed(seed)
+
+    os.makedirs(workdir, exist_ok=True)
+
+    classes_info = load_classes(repo_id=dataset_repo_id)
+    with open(os.path.join(workdir, 'classes.json'), 'w') as f:
+        json.dump(classes_info.classes, f)
+    checkpoints = os.path.join(workdir, 'checkpoints')
+    last_ckpt_dir = os.path.join(checkpoints, 'last')
+    model_args = dict(model_args or {})
+    model_args = {**model_args}
+    if os.path.exists(os.path.join(last_ckpt_dir, 'model.safetensors')):
+        if accelerator.is_main_process:
+            logging.info(f'Loading last checkpoint from {last_ckpt_dir!r} ...')
+        model_step = ModelStep.load_from_dir(last_ckpt_dir)
+
+        previous_epoch = model_step.epoch
+        if accelerator.is_main_process:
+            logging.info(f'Resume from epoch {previous_epoch!r}.')
+    else:
+        if accelerator.is_main_process:
+            logging.info(f'No last checkpoint found, initialize {timm_model_name!r} model '
+                         f'with {plural_word(len(tags_info.tags), "tag")}.')
+
+        model = Model.new(
+            model_name=timm_model_name,
+            tags=tags_info.tags,
+            pretrained=True,
+            pretrained_cfg=pretrained_cfg,
+            model_args=model_args,
+        )
+        previous_epoch = 0
+
+    model: Model
+    pretrained_tag = load_pretrained_tag(dataset_repo_id)
+    logging.info(f'Pretrained tag {pretrained_tag!r} found for dataset {dataset_repo_id!r}.')
--- animetrans/model/step.py
+++ animetrans/model/step.py
@@ -1,3 +1,4 @@
+import glob
 import json
 import os
 from dataclasses import dataclass
@@ -18,11 +19,20 @@ class ModelStep:
     def save_to_dir(self, directory: str):
         os.makedirs(directory, exist_ok=True)
         self.model.save_pretrained(directory)
+        metrics_to_save = {}
+        images_to_save = {}
+        for key, value in self.metrics.items():
+            if not isinstance(value, Image.Image):
+                metrics_to_save[key] = value
+            else:
+                images_to_save[key] = value
         with open(os.path.join(directory, 'metrics.json'), 'w') as f:
             json.dump({
                 'epoch': self.epoch,
-                **self.metrics,
+                **metrics_to_save,
             }, f, sort_keys=True, ensure_ascii=False, indent=4)
+        for key, value in images_to_save.items():
+            value.save(os.path.join(directory, f'{key}.png'))
 
     @classmethod
     def load_from_dir(cls, directory: str) -> 'ModelStep':
@@ -30,6 +40,12 @@ class ModelStep:
         with open(os.path.join(directory, 'metrics.json'), 'r') as f:
             metrics = json.load(f)
         epoch = metrics.pop('epoch')
+        for png_file in glob.glob(os.path.join(directory, '*.png')):
+            key = os.path.splitext(os.path.basename(png_file))[0]
+            image = Image.open(png_file)
+            image.load()
+            metrics[key] = image
+
         return cls(
             model=model,
             epoch=epoch,
