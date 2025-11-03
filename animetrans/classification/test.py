8e1985d0a02a61b021e67d89ab39f7f552b3273d
narugo1992 <narugo1992@deepghs.org>
2025-11-03 19:22:39 +0800

    dev(narugo): default not to force, fix

--- animetrans/classification/test.py
+++ animetrans/classification/test.py
@@ -27,16 +27,16 @@ def test(workdir: str, num_workers: int = 32, batch_size: int = 32, force: bool 
     eval_step_info = StepInfo.load_from_dir(model_ckpt_dir)
     metrics_dir = os.path.join(model_ckpt_dir, 'test')
 
-    if not force and os.path.exists(os.path.join(metrics_dir, 'metrics.json')):
-        test_step_info = StepInfo.load_from_dir(metrics_dir)
-        if test_step_info.epoch == eval_step_info.epoch:
-            logging.info(f'Already checkpoint {ckpt_name!r} tested for {workdir}, skipped.')
-            return
-
     accelerator = accelerator or Accelerator(
         # mixed_precision=self.cfgs.mixed_precision,
         step_scheduler_with_optimizer=False,
     )
+    if not force and os.path.exists(os.path.join(metrics_dir, 'metrics.json')):
+        test_step_info = StepInfo.load_from_dir(metrics_dir)
+        if test_step_info.epoch == eval_step_info.epoch:
+            if accelerator.is_main_process:
+                logging.info(f'Already checkpoint {ckpt_name!r} tested for {workdir}, skipped.')
+            return
 
     with open(os.path.join(workdir, 'meta.json'), 'r') as f:
         meta_info = json.load(f)
